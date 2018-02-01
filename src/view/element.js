36580de643ed525d056c6b15d6401e5e56b290ff
ps_fedotov <ps_fedotov@bongo-boom.ru>
2018-02-01 19:24:43 +0300

    class Element

--- src/view/element.js
+++ src/view/element.js
@@ -1,16 +1,55 @@
 import "pixi.js";
 
 export default class Element {
-    constructor(res, i){
+    constructor(res, i, fn){
         let sp = new PIXI.Sprite.fromImage(res);
         sp.texture.baseTexture.on('loaded', () =>{
             let {width, height} = sp;
             let k = width / 100;
-            sp.position = {x: 1780, y: i * 100 + 10};
+            sp.position = {x: 1400, y: i * 100 + 10};
             sp.width = width / k;
             sp.height = height / k;
-            sp.interactive = true;
         });
+        sp.interactive = true;
+        this.index = i;
+        this.res = res;
         this.sprite = sp;
+        this.fn = fn;
+
+        this.onTouchStart = this.onTouchStart.bind(this);
+        this.onTouchMove = this.onTouchMove.bind(this);
+        this.onTouchEnd = this.onTouchEnd.bind(this);
+        this.onTouchEndOutside = this.onTouchEndOutside.bind(this);
+
+        this.addEvents();
+    }
+
+    addEvents(){
+        this.sprite.on("touchstart", this.onTouchStart);
+        this.sprite.on("touchmove", this.onTouchMove);
+        this.sprite.on("touchend", this.onTouchEnd);
+    }
+
+    removeEvents(){
+
+    }
+
+    onTouchStart(e){
+        this.data = e.data;
+        this.dragging = true;
+        console.log('aaa');
+        // this.fn.stopPropagation();
+    }
+
+    onTouchMove(e){
+        console.log('aaa');
+    }
+
+    onTouchEnd(e){
+        console.log('aaa');
+    }
+
+    onTouchEndOutside(e){
+
     }
 }
--- src/view/index.js
+++ src/view/index.js
@@ -12,17 +12,25 @@ export default class View {
     constructor(element, res){
         this.element = element;
         this.res = res;
+        this.fn = {
+            stopPropagation: this.stopPropagationAllElements.bind(this)
+        }
+    }
+
+    stopPropagationAllElements(){
+        alert("work");
     }
 
     setCanvas(){
         let {clientHeight, clientWidth} = document.documentElement;
-        this.app = new PIXI.Application(1920, 864, {backgroundColor : 0x1099bb});
+        this.app = new PIXI.Application(1536, 864, {backgroundColor : 0x1099bb});
         this.element.appendChild(this.app.view);
         this.res.forEach((res, i) => {
-            let e = new Element(res, i);
+            let e = new Element(res, i, this.fn);
             this.app.stage.addChild(e.sprite);
         });
-
+        this.app.stage.interactiveChildren = true;
+        this.app.stage.interactive = true;
         this.scene = new PIXI.Container();
         this.scene.position = {x: scene.x, y: scene.y};
         this.scene.width = scene.width;
@@ -31,7 +39,8 @@ export default class View {
         bg.beginFill(0x000000, 1);
         bg.drawRect(scene.x, scene.y, scene.width, scene.height);
         this.app.stage.addChild(bg);
-        this.app.stage.addChild(this.scene)
+        this.app.stage.addChild(this.scene);
+
     }
 
     setModel(model){
