cdb70bc8e24b93112aa578a28fde7ce2df662db7
ps_fedotov <ps_fedotov@bongo-boom.ru>
2018-02-01 18:18:59 +0300

    load files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import View from './view';
 import Model from './model';
 import "./styles/style.scss";
+import {content} from "../res.json";
 
-let app = document.querySelector("#app");
+const app = document.querySelector("#app");
+const res = content.assets;
 
 const model = new Model();
-const view = new View(app);
+const view = new View(app, res);
 view.setModel(model);
 view.setCanvas();
--- src/view/index.js
+++ src/view/index.js
@@ -1,14 +1,20 @@
 import "pixi.js";
 
 export default class View {
-    constructor(app){
+    constructor(app, res){
         this.app = app;
+        this.res = res;
     }
 
     setCanvas(){
         let {clientHeight, clientWidth} = document.documentElement;
         this.game = new PIXI.Application(1536, 864, {backgroundColor : 0x1099bb});
         this.app.appendChild(this.game.view);
+        this.res.forEach((res, i) => {
+            let sp = new PIXI.Sprite.fromImage(res);
+            sp.position = {x: i * 10, y: i * 10};
+            this.game.stage.addChild(sp);
+        });
     }
 
     setModel(model){
